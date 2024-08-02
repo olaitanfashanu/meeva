dc4d9aef16655dd4328eaa191d745ca0541939cf
Olaitan Fashanu <84292757+olaitanfashanu@users.noreply.github.com>
2024-08-02 16:43:07 +0300

    new

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function loadHTML(url, elementId) {
+    fetch(url).then(response => response.text()).then(data => {
+        document.getElementById(elementId).innerHTML = data;}).catch(error =>
+            console.error('Error loading HTML:', error));
+
+}
+
+document.addEventListener("DOMContentLoaded",function() {
+    loadHTML('header.html', 'header');
+    loadHTML('footer.html', 'footer');
+});
+
+
+
+
+
+
 document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault();
     const formData = {
