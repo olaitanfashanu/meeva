87a1753d6aca58c3432b126078e79cf1c4b03b3d
Olaitan Fashanu <84292757+olaitanfashanu@users.noreply.github.com>
2024-08-11 18:03:57 +0300

    form handling fix

--- script.js
+++ script.js
@@ -21,13 +21,12 @@ document.addEventListener("DOMContentLoaded",function() {
 
 
 
-
-document.getElementById('form').addEventListener('submit', function(event) {
-    event.preventDefault();
+function submitForm() {
     const formData = {
         name: document.getElementById('name').value,
         email: document.getElementById('email').value
     };
+}
     
     fetch('/submit', {
         method: 'POST',
@@ -43,4 +42,33 @@ document.getElementById('form').addEventListener('submit', function(event) {
     .catch(error => {
         console.error('Error:', error);
     });
-});
+
+
+
+function submitFormContact() {
+    const formData = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        tel: document.getElementById('tel').value,
+        text: document.getElementById('text').value
+
+    };
+    }
+        
+    fetch('/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+    
+    
+       
